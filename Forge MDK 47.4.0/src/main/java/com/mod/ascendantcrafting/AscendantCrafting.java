7675a5774a8298a34f1a1a0928a6f89becd774f5
Gytorc <44067974+Gytorc@users.noreply.github.com>
2025-09-02 09:21:32 -0500

    Version 0.1.0
    formatted and included Primary class and mods.toml

---
+++ Forge MDK 47.4.0/src/main/java/com/mod/ascendantcrafting/AscendantCrafting.java
@@ -0,0 +1,12 @@
+package com.yourname.ascendantcrafting;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(AscendantCrafting.MODID)
+public class AscendantCrafting {
+    public static final String MODID = "ascendantcrafting"; // must match mods.toml
+
+    public AscendantCrafting() {
+        // Hook registries here later (DeferredRegisters, events, etc.)
+    }
+}
